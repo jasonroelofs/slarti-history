f2049266e91b29edafbe6bbca5928f23d8bf48b4
Jason Roelofs <jameskilton@gmail.com>
2011-03-01 13:15:29 -0500

    Move camera and transform managers into the managers namespace

--- include/managers/TransformManager.h
+++ include/managers/TransformManager.h
@@ -5,48 +5,50 @@
 
 #include <OgreSceneManager.h>
 
-using namespace components;
+namespace managers {
+  using namespace components;
 
-/**
- * Manager for all Transform components, and thus
- * all Actors. Deals with translating data from Actors
- * to Ogre::SceneNodes and back.
- *
- * May just make this the top level Actor management class.
- */
-class TransformManager { // : public ComponentManager<TransformComponent>
-  public:
+  /**
+   * Manager for all Transform components, and thus
+   * all Actors. Deals with translating data from Actors
+   * to Ogre::SceneNodes and back.
+   *
+   * May just make this the top level Actor management class.
+   */
+  class TransformManager { // : public ComponentManager<TransformComponent>
+    public:
 
-    /**
-     * Needs to know about the current scene manager
-     */
-    TransformManager(Ogre::SceneManager* manager);
+      /**
+       * Needs to know about the current scene manager
+       */
+      TransformManager(Ogre::SceneManager* manager);
 
-    /**
-     * We've got a new component, initialize
-     * it with Ogre and the system as a whole.
-     */
-    void initialize(TransformComponent* transform);
+      /**
+       * We've got a new component, initialize
+       * it with Ogre and the system as a whole.
+       */
+      void initialize(TransformComponent* transform);
 
-    /**
-     * Update all actors
-     */
-    void update();
+      /**
+       * Update all actors
+       */
+      void update();
 
-  protected:
+    protected:
 
-    /**
-     * Current active scene manager
-     */
-    Ogre::SceneManager* mSceneManager;
+      /**
+       * Current active scene manager
+       */
+      Ogre::SceneManager* mSceneManager;
 
-    /**
-     * List of all TransformComponents currently in
-     * the system
-     */
-    std::vector<TransformComponent*> mComponents;
+      /**
+       * List of all TransformComponents currently in
+       * the system
+       */
+      std::vector<TransformComponent*> mComponents;
 
-};
+  };
 
+}
 #endif // __TRANSFORM_MANAGER_H__
 
--- src/managers/TransformManager.cpp
+++ src/managers/TransformManager.cpp
@@ -1,19 +1,23 @@
 #include "managers/TransformManager.h"
 
-TransformManager::TransformManager(Ogre::SceneManager* manager)
-  : mSceneManager(manager)
-{
-}
+namespace managers {
 
-void TransformManager::initialize(TransformComponent* component) {
-  mComponents.push_back(component);
+  TransformManager::TransformManager(Ogre::SceneManager* manager)
+    : mSceneManager(manager)
+  {
+  }
 
-  // Figure out where to add the SceneNode for this component.
-  // We'll want to look at the Actor's parent, if one exists,
-  // otherwise it's pulled off of the root scene node
-}
+  void TransformManager::initialize(TransformComponent* component) {
+    mComponents.push_back(component);
 
-void TransformManager::update() {
-  // Update all Actors, copy their current position, rotation, and scale
-  // into their scene node
+    // Figure out where to add the SceneNode for this component.
+    // We'll want to look at the Actor's parent, if one exists,
+    // otherwise it's pulled off of the root scene node
+  }
+
+  void TransformManager::update() {
+    // Update all Actors, copy their current position, rotation, and scale
+    // into their scene node
+  }
+
 }
