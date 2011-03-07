da289d9bdf862a44d1d1929282246e4059b0e1bd
Jason Roelofs <jameskilton@gmail.com>
2011-03-06 20:13:00 -0500

    Transforms get added to the scene now, cameras get positioned according to the actor they are 'attached' to

--- src/managers/TransformManager.cpp
+++ src/managers/TransformManager.cpp
@@ -1,4 +1,5 @@
 #include "managers/TransformManager.h"
+#include "components/TransformComponent.h"
 
 #include <OgreSceneManager.h>
 
@@ -14,6 +15,8 @@ namespace managers {
     // Figure out where to add the SceneNode for this component.
     // We'll want to look at the Actor's parent, if one exists,
     // otherwise it's pulled off of the root scene node
+    component->_sceneNode = mSceneManager->getRootSceneNode()->createChildSceneNode(); 
+    component->_sceneNode->setPosition(component->position);
   }
 
   void TransformManager::remove(TransformComponent* component) {
@@ -22,6 +25,8 @@ namespace managers {
   void TransformManager::update() {
     // Update all Actors, copy their current position, rotation, and scale
     // into their scene node
+    //_sceneNode.setPosition(component->position);
+    //_sceneNode.setScale(component->scale);
   }
 
   MANAGER_IMPLEMENTATION(Transform)
