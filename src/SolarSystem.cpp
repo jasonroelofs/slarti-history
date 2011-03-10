7d7b2ca399bfb6ac78d1edc11fd7591e9f17d699
Jason Roelofs <jameskilton@gmail.com>
2011-03-10 13:06:05 -0500

    Probing the capabilities of floats with a static origin

--- src/SolarSystem.cpp
+++ src/SolarSystem.cpp
@@ -22,7 +22,28 @@ void SolarSystem::generatePlanets() {
 }
 
 void SolarSystem::generatePlanet() {
-  Actor* planet = new Actor(Ogre::Vector3(0, 0, 120000));
+  Actor* planet = new Actor(Ogre::Vector3(0, 0, 7500000));
   planet->transform->scale = Ogre::Vector3(2000, 2000, 2000);
   planet->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
+
+  // Reference planets, made super huge so I can see them
+  Actor* planet2 = new Actor(Ogre::Vector3(7500000, 0, 0));
+  planet2->transform->scale = Ogre::Vector3(80000, 80000, 80000);
+  planet2->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
+
+  Actor* planet3 = new Actor(Ogre::Vector3(0, 7500000, 0));
+  planet3->transform->scale = Ogre::Vector3(100000, 100000, 100000);
+  planet3->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
+
+  Actor* planet4 = new Actor(Ogre::Vector3(0, 0, -7500000));
+  planet4->transform->scale = Ogre::Vector3(2000, 2000, 2000);
+  planet4->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
+
+  Actor* planet5 = new Actor(Ogre::Vector3(-7500000, 0, 0));
+  planet5->transform->scale = Ogre::Vector3(80000, 80000, 80000);
+  planet5->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
+
+  Actor* planet6 = new Actor(Ogre::Vector3(0, -7500000, 0));
+  planet6->transform->scale = Ogre::Vector3(100000, 100000, 100000);
+  planet6->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
 }
