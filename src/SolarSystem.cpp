4afa5686ef8473d462be80f590525ebb013b4e36
Jason Roelofs <jameskilton@gmail.com>
2011-03-14 20:29:46 -0400

    Settling in on a scale and distance for planets and stars, building up a system using distances and orbits

--- src/SolarSystem.cpp
+++ src/SolarSystem.cpp
@@ -15,17 +15,30 @@ void SolarSystem::generate() {
 }
 
 void SolarSystem::generateStars() {
-  // int numStars = 1;
-
   mStars.push_back(StarGenerator::generateStar());
 }
 
 void SolarSystem::generatePlanets() {
-  int numPlanets = Ogre::Math::RangeRandom(1, 6);
+  int numPlanets = Ogre::Math::RangeRandom(0, 12);
+  int orbitDiffMin = 200000;
+  int orbitDiffMax = 500000;
+
   Actor* planet;
+  int orbit = 100000;
+  float angle;
 
   for(int i = 0; i < numPlanets; i++) {
     planet = PlanetGenerator::generatePlanet();
+
+    orbit += Ogre::Math::RangeRandom(orbitDiffMin, orbitDiffMax);
+    angle = Ogre::Math::RangeRandom(0, 360);
+
+    planet->transform->position =
+      Ogre::Vector3(
+          orbit * Ogre::Math::Cos(Ogre::Degree(angle)),
+          Ogre::Math::RangeRandom(-100000, 100000),
+          orbit * Ogre::Math::Sin(Ogre::Degree(angle))
+          );
 
     cout << "Planet generated at " << planet->transform->position << endl;
 
--- src/generators/PlanetGenerator.cpp
+++ src/generators/PlanetGenerator.cpp
@@ -5,20 +5,10 @@
 
 #include <OgreMath.h>
 
-const int PlanetGenerator::MIN_PLANET_SIZE = 2000;
-const int PlanetGenerator::MAX_PLANET_SIZE = 10000;
+const int PlanetGenerator::MIN_PLANET_SIZE = 10000;
+const int PlanetGenerator::MAX_PLANET_SIZE = 50000;
 
-Actor* PlanetGenerator::generatePlanet() { //Ogre::Vector3 sector, Ogre::Vector3 offset, float size) {
-
-  // choose sector to place planet
-  // choose offset of sector
-  // choose size of planet
-
-  Ogre::Vector3 sector(
-    Ogre::Math::RangeRandom(1, 10),
-    Ogre::Math::RangeRandom(1, 10),
-    Ogre::Math::RangeRandom(1, 10)
-  );
+Actor* PlanetGenerator::generatePlanet() {
 
   Ogre::Vector3 offset(
     Ogre::Math::UnitRandom(),
@@ -28,7 +18,7 @@ Actor* PlanetGenerator::generatePlanet() { //Ogre::Vector3 sector, Ogre::Vector3
 
   int size = Ogre::Math::RangeRandom(MIN_PLANET_SIZE, MAX_PLANET_SIZE);
   
-  Actor* planet = new Actor(utils::calculatePosition(sector, offset));
+  Actor* planet = new Actor();
   planet->transform->scale = Ogre::Vector3(size, size, size);
   planet->addComponent(new components::MeshComponent("sphere.mesh", "dirt"));
 
