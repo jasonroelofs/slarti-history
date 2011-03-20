62d1b8275ff4f042d155b5626f4e7dc7307bba7c
Jason Roelofs <jameskilton@gmail.com>
2011-03-20 15:15:23 -0400

    Add units to UI

--- src/ui/UIManager.cpp
+++ src/ui/UIManager.cpp
@@ -79,14 +79,14 @@ namespace ui {
   void UIManager::updateVelocity(const Ogre::Real& newV) {
     Rocket::Core::Element* e = mRocketContext->GetDocument("game_window")->GetElementById("velocity");
     if(e) {
-      e->SetInnerRML(Rocket::Core::String(128, "Speed: %0.2f", newV).CString());
+      e->SetInnerRML(Rocket::Core::String(128, "Speed: %0.2f km/s", newV).CString());
     }
   }
 
   void UIManager::updateAcceleration(const Ogre::Real& newA) {
     Rocket::Core::Element* e = mRocketContext->GetDocument("game_window")->GetElementById("acceleration");
     if(e) {
-      e->SetInnerRML(Rocket::Core::String(128, "Accel: %0.2f", newA).CString());
+      e->SetInnerRML(Rocket::Core::String(128, "Accel: %0.2f km/s^2", newA).CString());
     }
   }
 
