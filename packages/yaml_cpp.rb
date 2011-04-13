2f20f581d29274e605240cb0928155cbf165d7a2
Jason Roelofs <jameskilton@gmail.com>
2011-04-13 12:52:35 -0400

    Using the yaml-cpp library

---
+++ packages/yaml_cpp.rb
@@ -0,0 +1,18 @@
+package :yaml_cpp do |yaml|
+  yaml.download = "http://yaml-cpp.googlecode.com/files/yaml-cpp-0.2.6.tar.gz"
+  yaml.download_to = "yaml-cpp-0.2.6.tar.gz"
+
+  yaml.unpack = "tar jxvf"
+  yaml.unpack_to = "yaml-cpp"
+
+  yaml.build do |opts|
+    opts.all do
+      mkdir "Build"
+      cd "Build" do
+        sh "cmake -DBUILD_SHARED_LIBS=FALSE -DCMAKE_INSTALL_PREFIX=#{opts.prefix} ../"
+        sh "make -j 3"
+        sh "make install"
+      end
+    end
+  end
+end
