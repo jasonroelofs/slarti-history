cbb3cae9af8c4227a55d8fd076f916724028f955
Jason Roelofs <jameskilton@gmail.com>
2011-06-22 22:21:21 -0400

    Playing with textures and lights

--- src/org/slartibartfast/App.java
+++ src/org/slartibartfast/App.java
@@ -4,13 +4,17 @@ import org.slartibartfast.assets.loaders.PrefabLoader;
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
+import com.jme3.light.PointLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.debug.WireBox;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.util.TangentBinormalGenerator;
 import jme3tools.optimize.GeometryBatchFactory;
 import org.slartibartfast.prefabs.Hallway;
 import org.slartibartfast.prefabs.Panel;
@@ -20,6 +24,12 @@ import org.slartibartfast.prefabs.Panel;
  * @author roelofs
  */
 public class App extends SimpleApplication {
+  
+  private PointLight light1;
+  private PointLight light2;
+  private PointLight light3;
+  private PointLight light4;
+  private PointLight light5;
 
   @Override
   public void simpleInitApp() {
@@ -64,34 +74,34 @@ public class App extends SimpleApplication {
     }
     
     // And ceiling
-    y = 2.0f;
-    for(float x = -5.0f; x <= 5.0f; x += 1.0f) {
-      for(float z = -5.0f; z <= 5.0f; z += 1.0f) {
-        buildBoxAt(station, new Vector3f(x, y, z));
-      }
-    }
+//    y = 1.0f;
+//    for(float x = -5.0f; x <= 5.0f; x += 1.0f) {
+//      for(float z = -5.0f; z <= 5.0f; z += 1.0f) {
+//        buildBoxAt(station, new Vector3f(x, y, z));
+//      }
+//    }
     
     // 4 walls
     
-    for(y = -1.0f; y <= 1.0f; y += 1.0f) {
+    for(y = -1.0f; y < 1.0f; y += 1.0f) {
       for(float x = -5.0f; x <= 5.0f; x += 1.0f) {
         buildBoxAt(station, new Vector3f(x, y, -6.0f));
       }
     }
     
-    for(y = -1.0f; y <= 1.0f; y += 1.0f) {
+    for(y = -1.0f; y < 1.0f; y += 1.0f) {
       for(float x = -5.0f; x <= 5.0f; x += 1.0f) {
         buildBoxAt(station, new Vector3f(x, y, 6.0f));
       }
     }
     
-   for(y = -1.0f; y <= 1.0f; y += 1.0f) {
+   for(y = -1.0f; y < 1.0f; y += 1.0f) {
       for(float z = -5.0f; z <= 5.0f; z += 1.0f) {
         buildBoxAt(station, new Vector3f(6.0f, y, z));
       }
     }
    
-   for(y = -1.0f; y <= 1.0f; y += 1.0f) {
+   for(y = -1.0f; y < 1.0f; y += 1.0f) {
       for(float z = -5.0f; z <= 5.0f; z += 1.0f) {
         buildBoxAt(station, new Vector3f(-6.0f, y, z));
       }
@@ -99,19 +109,69 @@ public class App extends SimpleApplication {
 
    
     Node optimized = GeometryBatchFactory.optimize(station);
+    TangentBinormalGenerator.generate(optimized);
     getRootNode().attachChild(optimized);
+    
+    // Add Lights
+    light1 = addLightAt(new Vector3f(-4f, 0f, -4f));
+    light2 = addLightAt(new Vector3f(4f, 0f, -4f));
+    light3 = addLightAt(new Vector3f(4f, 0f, 4f));
+    light4 = addLightAt(new Vector3f(-4f, 0f, 4f));
+    light5 = addLightAt(Vector3f.ZERO);
+  }
+  
+  private PointLight addLightAt(Vector3f position) {
+    PointLight light;
+    light = new PointLight();
+    light.setPosition(position);
+    light.setRadius(5.0f);
+    
+    Geometry lightGeo = new Geometry("sphere", new Box(position, 0.1f, 0.1f, 0.1f));
+    Material geoMat = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+    lightGeo.setMaterial(geoMat);
+    getRootNode().attachChild(lightGeo);
+    
+    getRootNode().addLight(light);
+    
+    return light;
   }
   
   private void buildBoxAt(Node node, Vector3f position) {
     Box box = new Box(position, 0.5f, 0.5f, 0.5f);
     Geometry boxGeo = new Geometry("wall", box);
-    Material boxMat = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-    boxMat.setColor("Color", ColorRGBA.Blue);
-    boxMat.getAdditionalRenderState().setWireframe(true);
+    
+    Material boxMat = new Material(getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
+    //boxMat.setColor("Color", ColorRGBA.Blue);
+    //boxMat.getAdditionalRenderState().setWireframe(true);
+    boxMat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/Terrain/Pond/Pond.png"));
+    //boxMat.setTexture("NormalMap", assetManager.loadTexture("Textures/Terrain/Pond/Pond_normal.png"));
+    boxMat.setFloat("Shininess", 2f);
+    boxMat.setBoolean("UseMaterialColors", true);
+    boxMat.setColor("Ambient", ColorRGBA.Yellow);
+    boxMat.setColor("Diffuse", ColorRGBA.Red);
+    boxMat.setColor("Specular", ColorRGBA.Blue);
+    
     boxGeo.setMaterial(boxMat);
-    //boxGeo.setLocalTranslation(position);
     
     node.attachChild(boxGeo);
+  }
+  private float timeElapsed = 0.0f;
+  @Override
+  public void simpleUpdate(float tpf) {
+    float omega = 2f * 2 * 3.1415f;
+    
+    float xDiff = 0.1f * FastMath.sin2(omega * timeElapsed);
+    
+    Vector3f oldPos = light1.getPosition().clone();
+    
+    System.out.println("change this frame: " + xDiff);
+    System.out.println("Second counter is: " + timeElapsed);
+    
+    oldPos.x = oldPos.x + xDiff;
+    
+    light1.setPosition(oldPos);
+    
+    timeElapsed += tpf;
   }
 
   private void initResourceManagement() {
