b0b9fa47a5162b864bee0b65ada3d8d005abb222
Jason Roelofs <jameskilton@gmail.com>
2011-07-25 13:05:52 -0400

    Allow creation of an Actor with default location

--- src/org/slartibartfast/ActorManager.java
+++ src/org/slartibartfast/ActorManager.java
@@ -1,5 +1,6 @@
 package org.slartibartfast;
 
+import com.jme3.math.Vector3f;
 import org.slartibartfast.behaviors.PhysicalBehavior;
 
 /**
@@ -17,6 +18,17 @@ public class ActorManager implements InputReceiver {
   public Actor create() {
     Actor a = new Actor();
     a.useBehavior(new PhysicalBehavior());
+    return a;
+  }
+
+  /**
+   * Build an actor and give it a starting location
+   */
+  public Actor create(Vector3f startingLocation) {
+    Actor a = create();
+    PhysicalBehavior b = a.getBehavior(PhysicalBehavior.class);
+    b.setLocation(startingLocation);
+
     return a;
   }
 
--- test/org/slartibartfast/ActorManagerTest.java
+++ test/org/slartibartfast/ActorManagerTest.java
@@ -1,5 +1,6 @@
 package org.slartibartfast;
 
+import com.jme3.math.Vector3f;
 import org.slartibartfast.behaviors.PhysicalBehavior;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -20,6 +21,15 @@ public class ActorManagerTest {
   public void createReturnsAnActor() {
     Actor a = manager.create();
     assertNotNull(a);
+  }
+
+  @Test
+  public void canCreateActorAndGiveInitialLocation() {
+    Vector3f location = new Vector3f(2.0f, 3.0f, 4.0f);
+    Actor a = manager.create(location);
+    PhysicalBehavior b = a.getBehavior(PhysicalBehavior.class);
+
+    assertEquals(location, b.getLocation());
   }
 
   @Test
