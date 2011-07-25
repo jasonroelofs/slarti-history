60bb749a000223a3e3d9c41063330a1bdb59619c
Jason Roelofs <jameskilton@gmail.com>
2011-07-24 21:14:35 -0400

    Change back to SimpleApplication
    The name could use a change, it does a lot that needs to happen for jme3
    to work properly. Also added the teapot to have something to look at
    until I get geometry showing up.

--- src/org/slartibartfast/App.java
+++ src/org/slartibartfast/App.java
@@ -1,29 +1,26 @@
 package org.slartibartfast;
 
-import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  *
  * @author roelofs
  */
-public class App extends Application {
+public class App extends SimpleApplication {
 
   private ActorManager actorManager;
   private InputSystem inputSystem;
 
   @Override
-  public void initialize() {
-    super.initialize();
-
+  public void simpleInitApp() {
     actorManager = new ActorManager();
     inputSystem = new InputSystem(getInputManager());
     inputSystem.setInputReceiver(actorManager);
-
-    // Init world / resources
-    // Init Camera
-    // Camera cam = new Camera(getCamera());
-    //Actor camera = ActorManager.create();
-    //camera.useBehavior(Camera);
 
     /**
      *  Init the player.
@@ -33,11 +30,31 @@ public class App extends Application {
      * - Set starting location and orientation
      */
     Actor player = actorManager.create();
-  }
 
-  @Override
-  public void update() {
-    super.update();
+    Actor camera = actorManager.create();
+
+    //CameraBehavior cam = new CameraBehavior(getCamera());
+    //camera.useBehavior(new CameraBehavior(getCamera()));
+
+    /**
+     * Use JME tutorial to give us something to look at
+     */
+    Spatial teapot = getAssetManager().loadModel(
+            "Models/Teapot/Teapot.obj");
+    Material mat = new Material(
+            getAssetManager(),
+            "Common/MatDefs/Misc/ShowNormals.j3md");
+    teapot.setMaterial(mat);
+
+    getRootNode().attachChild(teapot);
+
+    DirectionalLight sun = new DirectionalLight();
+    sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
+    getRootNode().addLight(sun);
+
+    System.out.println("Camera is currently at " + getCamera().getLocation());
+    System.out.println("Camera is pointing at " + getCamera().getDirection());
+    System.out.println("And the teapot is currently at " + teapot.getLocalTransform());
   }
 
   public static void main(String[] args) {
