64fd5b71b0b5aebdc558ba02cd49f3ecbac34f4c
Jason Roelofs <jameskilton@gmail.com>
2011-07-30 21:36:45 -0400

    Default DirectionalLight to white

--- src/org/slartibartfast/behaviors/DirectionalLightBehavior.java
+++ src/org/slartibartfast/behaviors/DirectionalLightBehavior.java
@@ -1,10 +1,10 @@
 package org.slartibartfast.behaviors;
 
 import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import org.slartibartfast.Actor;
-import org.slartibartfast.Behavior;
 
 /**
  * Any actor given this behavior gives off a uniform directional
@@ -28,6 +28,7 @@ public class DirectionalLightBehavior extends LightBehavior {
   public void initialize(Actor actor, Object ... params) {
     DirectionalLight light = new DirectionalLight();
     light.setDirection(direction);
+    light.setColor(ColorRGBA.White);
     actor.get(Node.class, "node").getParent().addLight(light);
 
     initialized = true;
