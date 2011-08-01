565e1c8e3ff0eafa2b69f5d29d4712a4c24e86f3
Jason Roelofs <jameskilton@gmail.com>
2011-07-31 21:04:55 -0400

    Add simple wrapper class for key mappings

---
+++ src/org/slartibartfast/UserKeyMapping.java
@@ -0,0 +1,13 @@
+package org.slartibartfast;
+
+import java.util.HashMap;
+
+/**
+ * Map of events to keys, defined by the user.
+ *
+ * This is a wrapper class around a String,String hash map so we
+ * don't have to constantly type out the types.
+ */
+public class UserKeyMapping extends HashMap<Events, String> {
+
+}
