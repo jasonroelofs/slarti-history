0d88b9fb19ae56dc3806ccb379f657bc62b70b52
Jason Roelofs <jameskilton@gmail.com>
2011-09-09 22:26:06 -0400

    Update comments, some code cleanup

--- src/org/slartibartfast/behaviors/PhysicsBehavior.java
+++ src/org/slartibartfast/behaviors/PhysicsBehavior.java
@@ -1,15 +1,8 @@
 package org.slartibartfast.behaviors;
 
 import com.jme3.bullet.PhysicsSpace;
-import com.jme3.bullet.collision.shapes.BoxCollisionShape;
-import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.collision.shapes.CompoundCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.bullet.util.CollisionShapeFactory;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
 import com.jme3.scene.SceneGraphVisitor;
 import com.jme3.scene.Spatial;
 import org.slartibartfast.Behavior;
@@ -17,6 +10,12 @@ import org.slartibartfast.Behavior;
 /**
  * This behavior registers the Actor as a collidable entity
  * in the physics engine.
+ *
+ * NOTE: Once this behavior is put onto an Actor, any behavior
+ * that sets location or rotation on the actor WILL NOT WORK.
+ * Physics takes COMPLETE control over the location and rotation
+ * of the actor. To manually transform the actor you'll need to disable
+ * physics for this Actor, move the Actor, then re-enable physics.
  */
 public class PhysicsBehavior extends Behavior {
 
@@ -31,13 +30,6 @@ public class PhysicsBehavior extends Behavior {
 
   public float getMass() {
     return mass;
-  }
-
-  @Override
-  public void perform(float delta) {
-    // Probably need to update TransformBehavior with new
-    // location / rotation from Physics. Will deal with this when
-    // I need that information
   }
 
   class PhysicsApplier implements SceneGraphVisitor {
@@ -69,6 +61,8 @@ public class PhysicsBehavior extends Behavior {
     // properly before physics takes over
     actor.getBehavior(TransformBehavior.class).perform(1.0f);
 
+    // We want to build a collision hull for all Geometry nodes, as
+    // Bullet recommends more smaller hulls over a few large hulls
     actor.getNode().breadthFirstTraversal(new PhysicsApplier(physicsSpace));
 
     initialize();
